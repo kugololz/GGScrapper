f4a6d5d36019444099d79a9099783bbdf10f6db9
kugololz <yokoderayouto69@gmail.com>
2024-04-06 14:40:45 -0600

    Bugs arreglados en scrapper de Cyberpuerta

--- login_test.py
+++ login_test.py
@@ -1,20 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-login_url = 'https://covirsast.com/site/login-sesion'
-username = 'compras@grupordc.com.mx'
-password = '123'
+def decode(text):
+    return text.encode(sys.stdout.encoding, errors = 'replace').decode(sys.stdout.encoding)
 
-login_data = {
-    'usuario': username,
-    'contraseña': password
-}
+def login(driver):
+    
+    driver.get('https://me2.grupocva.com/me/inicio.php#')
 
-session = requests.Session()
+    login_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'top-login-nav-trigger')))
+    login_button.click()
 
-res = session.post(login_url, data = login_data)
+    login_form = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'frm')))
 
-if res.status_code == 200:
-    print('Inicio de sesión correcto')
-else:
-    print("Error al iniciar sesion")
+    #Completar el formulario de inicio de sesion
+    username_field = login_form.find_element(By.ID, 'fUsuario')
+    password_field = login_form.find_element(By.ID, 'fContrasenia')
+
+    # Credenciales de inicio de sesion
+    username_field.send_keys('admin73688')
+    password_field.send_keys('Juanpablo1645')
+
+    submit_button = login_form.find_element(By.ID, 'entrar')
+    submit_button.click()
+
+    WebDriverWait(driver, 10).until(EC.url_to_be('https://me2.grupocva.com/me/'))
+    print('Login successful')
+
+def scrape(driver, lista_de_busqueda):
+    
+    login(driver)
+    
+    for search_query in lista_de_busqueda:
+        search_input = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'busqueda_libre')))
+        search_input.clear()
+        search_input.send_keys(search_query)
+        search_input.send_keys(Keys.ENTER)
+        wait = WebDriverWait(driver, 100)
+        wait
+    
+    
+
+if __name__ == '__main__':
+    # Inicialización del driver
+    driver = webdriver.Chrome()
+    lista = [1,2,3]
+    scrape(driver, lista)
+
+    # CPU array catcher
+    excel_file = 'database.xlsx'
+    df = pd.read_excel(excel_file)
+
+    filtered_df = df[df['Categoria'].str.contains('CPU', case=False, na=False)]
+
+    cpu_skus = filtered_df['SKU']
+    cpu_skus_cleaned = cpu_skus.str.replace(r'\n', '', regex=True).str.replace('SKU: ', '')
+    cpu_list = []
+    for sku in cpu_skus_cleaned:
+        cpu_list.append(sku)
+        
+        
+
+    driver.quit()
